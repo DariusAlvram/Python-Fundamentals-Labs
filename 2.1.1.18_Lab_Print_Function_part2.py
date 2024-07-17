98c07ae5b6ca6a8bdfc0f27e1d115c2e30de4cbb
Dariusgrey <darioalvarezram@gmail.com>
2024-07-17 01:28:56 -0600

    Lab 2.1.1.18 print function part 2 completed

---
+++ 2.1.1.18_Lab_Print_Function_part2.py
@@ -0,0 +1,21 @@
+'''
+Estimated Time
+    5-10 minutes
+
+Difficulty level
+    Very easy
+
+Objectives
+    Become familiar with the function of print() and its formatting capabilities.
+    Experiment with the Python code.
+    Scenario
+    Modify the first line of code in the editor, using keywords sep and end, to match the expected result. Remember, use two functions print().
+
+    Do not change anything in the second invocation of print().
+
+Expected departure
+    Pregramming***Fundaments***in...Python
+'''
+
+print("Programming","Fundaments","in", sep="***", end="...")
+print("Python")
