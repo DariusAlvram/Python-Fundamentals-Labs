3d193ce9836413c477d78c2722db4bc461277fda
Dariusgrey <darioalvarezram@gmail.com>
2024-07-17 19:47:19 -0600

    Lab 2.2.1.19 Python Literals

--- Module_1_Labs/2.2.1.11_Lab_Python Literals - Chain.py
+++ Module_1_Labs/2.2.1.11_Lab_Python Literals - Chain.py
@@ -1,1 +1,23 @@
+'''
+Estimated Time
+    5-10 minutes
+
+Difficulty level
+    Easy
+
+Objectives
+    Become familiar with function print() and its formatting capabilities.
+    Practice encoding strings.
+    Experiment with the Python code.
+    Scenario
+    Write a single line of code, using the function print(), as well as new line and escape characters, to get the expected three-line output.
+
+Expected departure
+
+    "Estoy"
+    ""aprendiendo""
+    """Python"""
+'''
+
+
 print('"Estoy"\n""aprendiendo""\n"""Python"""')
