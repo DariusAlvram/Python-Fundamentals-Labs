074f5be9f46c9f96f6ac2d416f471c3d569c2b80
Dariusgrey <darioalvarezram@gmail.com>
2024-07-17 22:57:12 -0600

    2.5.1.2_Lab_Comments

---
+++ 2.5.1.2_Lab_Comments.py
@@ -0,0 +1,36 @@
+'''
+Estimated Time
+    5 minutes
+
+Difficulty level
+    Very easy
+
+Objectives
+    * Become familiar with the concept of comments in Python.
+    * Use and not use the comments.
+    * Replace comments with code.
+    * Experiment with the Python code.
+
+Scenario
+    The code in the editor contains comments. Try to improve it: add or remove comments where you consider it appropriate (sometimes removing a comment makes it more readable), in addition, rename variables where you think this will improve understanding of the code.
+
+NOTE
+
+    Comments are very important. They don't just make the show easier to understand, but they also serve to disable those parts of code that are not necessary (for example, when you need to test a certain part of the code, and ignore the rest). Good programmers describe every important part of the code, and give significant names to variables, because sometimes it is much easier to leave the comment within the code itself.
+
+    It is good to use variable names readable, and sometimes it is better divide the code in parts with names (for example in functions). In some situations, it is a good idea to write the steps of how the calculations were performed in a simple and clear way.
+
+    One more thing: it may happen that a comment contains an incorrect or erroneous piece of information, you should never do that on purpose.
+'''
+
+# This programs calculates the minutes within a determined number of hours
+
+a = 2          # Number of hours
+seconds = 3600 # Number of seconds within an hour
+
+print("Horas: ", a) #print the number of hours
+print("Segundos en Horas: ", a * seconds) # print the number of seconds in a determined number of hours
+
+#Here it should be printring "Good, bye", but the programmer didn't had time to complete it
+print("Good, bye!")
+#This is the end of the program that calculates the number of seconds in 2 hours
